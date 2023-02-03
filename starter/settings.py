b15a5128bae2d7dbf7e3f7ca62fca31331f23f2c
hp <wulfrykpierozek@gmail.com>
2023-02-03 09:53:06 +0100

    vercel

--- starter/settings.py
+++ starter/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'starter.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'itslewiosanotleviosa123!@34321',
